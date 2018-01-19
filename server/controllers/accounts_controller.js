0decf11e276ef997322e1bbc336579695bbf860d
bdlewis30 <32585973+bdlewis30@users.noreply.github.com>
2018-01-18 23:31:29 -0700

    Setting up checking controller.

--- server/controllers/accounts_controller.js
+++ server/controllers/accounts_controller.js
@@ -1,44 +1,44 @@
-module.exports = {
-    create: (req, res) => {
-        const db = req.app.get('db')
-        const {acct_name, acct_type, start_balance, interest, acct_num, routing_num, memo} = req.body;
+// module.exports = {
+//     create: (req, res) => {
+//         const db = req.app.get('db')
+//         const {acct_name, acct_type, start_balance, interest, acct_num, routing_num, memo} = req.body;
 
-        db.add_bank_account([acct_name, acct_type, start_balance, interest, acct_num, routing_num, memo])
-        .then(() => res.status(200).send())
-        .catch((error) => {
-            console.log(error)
-            res.status(500).send()
-        })
-    },
-    getOne: (req, res) => {
-        const db = req.app.get('db')
-        const {params} = req;
+//         db.add_bank_account([acct_name, acct_type, start_balance, interest, acct_num, routing_num, memo])
+//         .then(() => res.status(200).send())
+//         .catch((error) => {
+//             console.log(error)
+//             res.status(500).send()
+//         })
+//     },
+//     getOne: (req, res) => {
+//         const db = req.app.get('db')
+//         const {params} = req;
 
-        db.get_bank_account([params.id])
-        .then((account) => res.status(200).send(account))
-        .catch(() => res.status(500).send())
-    },
-    getAll: (req, res) => {
-        const db = req.app.get('db')
+//         db.get_bank_account([params.id])
+//         .then((account) => res.status(200).send(account))
+//         .catch(() => res.status(500).send())
+//     },
+//     getAll: (req, res) => {
+//         const db = req.app.get('db')
 
-        db.get_bank_accounts()
-        .then((accounts) => res.status(200).send(accounts))
-        .catch(() => res.status(500).send())
-    },
-    update: (req, res) => {
-        const db = req.app.get('db')
-        const {acct_name, acct_type, start_balance, interest, acct_num, routing_num, memo, acct_id} = req;
+//         db.get_bank_accounts()
+//         .then((accounts) => res.status(200).send(accounts))
+//         .catch(() => res.status(500).send())
+//     },
+//     update: (req, res) => {
+//         const db = req.app.get('db')
+//         const {acct_name, acct_type, start_balance, interest, acct_num, routing_num, memo, acct_id} = req;
 
-        db.update_bank_acct.update_account([acct_name, acct_type, start_balance, interest, acct_num, routing_num, memo])
-        .then(() => res.status(200).send())
-        .catch(() => res.status(500).send())
-    },
-    delete: (req, res) => {
-        const db = req.app.get('db')
-        const {params} = req
+//         db.update_bank_acct.update_account([acct_name, acct_type, start_balance, interest, acct_num, routing_num, memo])
+//         .then(() => res.status(200).send())
+//         .catch(() => res.status(500).send())
+//     },
+//     delete: (req, res) => {
+//         const db = req.app.get('db')
+//         const {params} = req
 
-        db.delete([params.id])
-        .then(() => res.status(200).send())
-        .catch(() => res.status(500).send())
-    }
-};
+//         db.delete([params.id])
+//         .then(() => res.status(200).send())
+//         .catch(() => res.status(500).send())
+//     }
+// };
--- server/controllers/checking_controller.js
+++ server/controllers/checking_controller.js
@@ -1,6 +1,37 @@
 module.exports = {
     create: (req, res) => {
         const db = req.app.get('db')
-        
-    }
-}
+        const {acct_name, acct_type, start_balance, acct_num, routing_num, memo} = req.body;
+
+        db.accounts.checking.create_checking([acct_name, acct_type, start_balance, acct_num, routing_num, memo])
+        .then(() => res.status(200).send())
+        .catch((error) => {
+            console.log(error)
+            res.status(500).send()
+        })
+    },
+    getAll: (req, res) => {
+        const db = req.app.get('db')
+
+        db.accounts.checking.read_all_checking()
+        .then((checking) => res.status(200).send(checking))
+        .catch(() => res.status(500).send())
+    },
+    getOne: (req, res) => {
+        const db = req.app.get('db')
+        const {params} = req
+
+        db.accounts.checking.read_one_checking([params.id])
+        .then((checking) => res.status(200).send(checking))
+        .catch(() => res.status(500).send())
+    }//,
+    // update: (req, res) => {
+    //     const db = req.app.get('db')
+
+    // },
+    // delete: (req, res) => {
+    //     const db = req.app.get('db')
+
+
+    // }
+};
